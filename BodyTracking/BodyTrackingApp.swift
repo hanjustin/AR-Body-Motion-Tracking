d25173b24fd8c3c31885790a9956691f95086970
Justin Lee <jhankilee@gmail.com>
2023-01-26 14:08:53 +0900

    Initial Commit

---
+++ BodyTracking/BodyTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTrackingApp.swift
+//  BodyTracking
+//
+//  Created by Justin Lee on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BodyTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
