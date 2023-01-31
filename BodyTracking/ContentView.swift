40e2f96c15bee1e1a6d72123823e639f5a41d1aa
Justin Lee <jhankilee@gmail.com>
2023-01-31 23:27:57 +0900

    Check if device supports body tracking

--- BodyTracking/ContentView.swift
+++ BodyTracking/ContentView.swift
@@ -6,11 +6,20 @@
 //
 
 import SwiftUI
+import ARKit
 
 struct ContentView: View {
     var body: some View {
-        ARViewContainer()
-            .edgesIgnoringSafeArea(.all)
+        if ARBodyTrackingConfiguration.isSupported {
+            ARViewContainer()
+                .edgesIgnoringSafeArea(.all)
+        } else {
+            Text(ErrorMsg.BodyTrackingUnsupported)
+        }
+    }
+    
+    private enum ErrorMsg {
+        static let BodyTrackingUnsupported = "AR body tracking is not supported on this device."
     }
 }
 
